4300d55cdf941e88047af63992b45999a51b1b0a
bgswe <bg.soft.eng@gmail.com>
2023-06-16 19:22:44 -0700

    upgrade react query, test fetch hook w/ react query

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import { RouterProvider } from 'react-router-dom'
 
 import { AuthenticationContext } from '@/authentication-context'
 import router from '@/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
     const [accessToken, setAccessToken] = useState(null)
 
     return (
-        <AuthenticationContext.Provider value={{ accessToken, setAccessToken }}>
-            <RouterProvider router={router} />
-        </AuthenticationContext.Provider>
+        <QueryClientProvider client={queryClient}>
+            <AuthenticationContext.Provider
+                value={{ accessToken, setAccessToken }}
+            >
+                <RouterProvider router={router} />
+            </AuthenticationContext.Provider>
+        </QueryClientProvider>
     )
 }
 
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,4 @@
-import useFetch from '@/api/fetch'
-import { useEffect } from 'react'
-
 const Dashboard = () => {
-    const fetchUser = useFetch('http://localhost:8000/auth/dummyneedsauth')
-
-    useEffect(() => {
-        fetchUser().then(console.log)
-    }, [])
-
     return <>dashboard</>
 }
 
